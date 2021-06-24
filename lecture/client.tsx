9c54b6f1b0ce99c23d1dbc3a1df3b0732d9e0887
Yongho5580 <Yongho5580@users.noreply.github.com>
2021-06-24 16:16:09 +0900

    16:16

--- lecture/client.tsx
+++ lecture/client.tsx
@@ -2,4 +2,4 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import GuGuDan from "./GuGuDan";
 
-ReactDOM.render(<GuGuDan />, document.querySelector('#root'))
+ReactDOM.render(<GuGuDan />, document.getElementById('root'));
