10cb0bb20d62d8dfcf4a04ee44d0f1d4497cefa9
Yongho5580 <Yongho5580@users.noreply.github.com>
2021-06-30 16:36:02 +0900

    Completed NumberBaseBall

--- lecture/client.tsx
+++ lecture/client.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { hot } from "react-hot-loader/root";
-import WordRelay from "./WordRelay";
+import NumberBaseBall from "./NumberBaseBall";
 
-const Hot = hot(WordRelay); // HOC
+const Hot = hot(NumberBaseBall); // HOC
 
 ReactDOM.render(<Hot />, document.getElementById("root"));
